43eb320cf29cbb5f3071f4892ac70ad98a3d7547
Tarun <tarun@Delwalt-Mac.local>
2024-08-22 12:44:53 +0530

    chore(husky): setup husky for commit message linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,14 +23,23 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './vite.config.ts'],
+    project: ['./tsconfig.json', './vite.config.ts', './commitlint.config.ts'],
   },
   plugins: ['react'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
     '@typescript-eslint/triple-slash-reference': 'off',
-    "@typescript-eslint/strict-boolean-expressions": "off"
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
   },
   settings: {
     react: {
