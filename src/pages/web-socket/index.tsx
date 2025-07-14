a41fa1a48ebed755494aee16b38f6c80b1d5038b
Rishi Garg <rishi@gkmit.co>
2025-07-14 16:29:33 +0530

    feat: show loading chat in center of screen

--- src/pages/web-socket/index.tsx
+++ src/pages/web-socket/index.tsx
@@ -5,17 +5,35 @@ const ChatApp = lazy(async () => await import('./chat-app'));
 const WebSocket = () => {
   return (
     <div style={styles.container}>
-      <Suspense fallback={<div>Loading chat...</div>}>
+      <Suspense
+        fallback={
+          <div style={styles.loaderWrapper}>
+            <div style={styles.loaderText}>Loading chat...</div>
+          </div>
+        }
+      >
         <ChatApp />
       </Suspense>
     </div>
   );
 };
 
-const styles = {
+const styles: Record<string, React.CSSProperties> = {
   container: {
     fontFamily: 'Arial, sans-serif',
     borderRadius: '8px',
+    height: '100vh', // Full screen height
+    width: '100vw', // Full screen width
+  },
+  loaderWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  loaderText: {
+    fontSize: '1.5rem',
+    color: 'white',
   },
 };
 
